50f2ad3e9ff16e4af57bdfd52e18b07326082cd2
Your Name <you@example.com>
2014-03-28 14:21:16 +0800

    first time init.

---
+++ sham_allithms/main.c
@@ -0,0 +1,18 @@
+//
+//  main.c
+//  sham_allithms
+//
+//  Created by cloud on 14-3-28.
+//  Copyright (c) 2014年 cloud. All rights reserved.
+//
+
+#include <stdio.h>
+
+int main(int argc, const char * argv[])
+{
+
+    // insert code here...
+    printf("Hello, World!\n");
+    return 0;
+}
+
